aacdc49cf54c677905142ba902c7295fd64eaa83
Siddhi-S <114423987+Siddhi-Sheth@users.noreply.github.com>
2023-08-24 22:42:25 +0530

    About

--- src/components/Features.js
+++ src/components/Features.js
@@ -30,8 +30,8 @@ const Features = () => {
       <h1 className="text-center text-5xl font-bold font-heading">FEATURES</h1>
       <div className="flex flex-wrap justify-center items-center gap-6 py-6">
         {features.map((feature, index) => (
-          <div key={index} className="group h-96 w-80 [perspective:1000px]">
-            <h1 className="absolute inset-x-0 bottom-10 text-3xl text-center">
+          <div key={index} className="group h-72 w-60 [perspective:1000px]">
+            <h1 className="absolute inset-x-0 bottom-10 text-xl text-center font-semibold text-lightBlack">
               {feature.title}
             </h1>
             <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
@@ -45,7 +45,7 @@ const Features = () => {
 
               <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-white [transform:rotateY(180deg)] [backface-visibility:hidden]">
                 <div className="flex min-h-full flex-col items-center justify-center">
-                  <h1 className="text-3xl font-bold">{feature.description}</h1>
+                  <h1 className="text-xl font-bold">{feature.description}</h1>
                 </div>
               </div>
             </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,58 +1,35 @@
 import React from "react";
-import "./About.css";
 
-const About = () => {
+const About2 = () => {
   return (
     <section
       id="about"
-      className="bg-blue-200 mt-20 w-full relative"
+      className="bg-darkWhite mt-20 w-full relative p-8"
       data-scroll-section
     >
-      <div className="flex flex-col md:flex-row justify-center items-center gap-3 py-28 px-10">
-        <div
-          className="blob w-full md:w-1/2 p-20 bg-blue-100 text-center"
-          data-scroll
-          data-scroll-speed="2"
-        >
-          <h1 className="text-4xl md:text-6xl font-bold font-heading">
-            Who are
-            <br />
-            we?
-          </h1>
+      <div className="flex justify-center items-center p-3 mb-5">
+        <h1 className="text-4xl font-bold font-heading text-darkIndigo">Who are we?</h1>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-3 w-full p-3 mt-3">
+        <div className="w-full flex justify-center items-center">
+            <img src="./images/Get_App.jpg" alt="app" className="w-[500px]"/>
         </div>
-        <div className="w-full flex flex-col justify-center items-center p-6 gap-3">
-          <div className="my-3">
-            <h3 className="text-3xl md:text-4xl font-semibold font-heading">
+        <div className="w-full flex flex-col justify-center items-center gap-6 p-3">
+        <h1 className="text-2xl font-semibold text-lightBlack">
               We are a women-led tech startup (previously known as Mynd) with an
               aim to be India's leading behavioral health platform for the
               youth.
-            </h3>
-          </div>
-          <div className="my-3">
-            <h6 className="text-xl md:text-2xl">
+            </h1>
+            <h1 className="text-xl text-lightBlack">
               We help you adapt to difficult situationsAnd we adapt to your
               unique needs. From activities that fit seamlessly into your daily
               routine to experienced therapists that will guide you
               <br /> -we have it all!
-            </h6>
-          </div>
+            </h1>
         </div>
-      </div>
-      <div class="wave">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
-          ></path>
-        </svg>
       </div>
     </section>
   );
 };
 
-export default About;
+export default About2;
