4c6b682b77c576d10fa8662ff707f0aebd9531c6
Siddhi-S <114423987+Siddhi-Sheth@users.noreply.github.com>
2023-08-24 21:45:47 +0530

    navbar-testimonial

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,11 +4,40 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import "./Testimonial.css";
 
 const Testimonial = () => {
+  const testimonial = [
+    {
+      content:
+        "My experience with Mynd has been really smooth and helpful. I’m happy I decided to reach out to them for their services and since the beginning they’ve been really involved and proactive. I always wanted to begin therapy but felt confused and frustrated on how to go about finding a suitable counsellor. Their platform has made the entire process so easy and hassle free.",
+    },
+    {
+      content:
+        "I was going through a really tough time and I was really struggling to find the help I needed until I found Mynd. They were so amazingly understanding, efficient and responsive, and made sure I got the help I needed as quickly as possible. Their discounted student rates were also incredibly helpful for me and it made it much easier for me to seek help. I definitely recommend seeking help through Mynd, it changed a lot of things for me and I’m very grateful for it.",
+    },
+    {
+      content:
+        "Mynd helped me with student-pocket friendly therapy session when I was in a rough spot. The team replied immediately and assigned a session (with an amazing psychologist/therapist) at my convenience and by the earliest. Plus the team is sensitive, helpful and punctual and offer assistance and reply ASAP (which is amazing). Sessions provided me with perspective and made me introspect and I’ve seen a positive impact on my life.",
+    },
+    {
+      content:
+        "My experience with mynd was wonderful. In a low point in my life and mindset, Ms. Vedika really was a source of self-reflection and understanding. Mynd also made the whole customer experience really smooth and stress free- important when dealing with people’s mental health. Overall, I would recommend this service for anyone that wants an understanding and empathetic ear and guidance",
+    },
+    {
+      content:
+        "At first, the thought of online therapy was a little scary. The thought of not having an in person meeting with someone who I was to share very personal experiences with made me anxious. Mynd has completely turned this thought around. Ms. Shubhangi has been a blessing during these tough times. Couldn’t be happier that I chose to take the plunge and start therapy!",
+    },
+  ];
+
   return (
-    <section className="relative p-6 md:p-16 w-full text-center bg-darkWhite" data-scroll-section>
-      <h1 className="text-3xl md:text-5xl font-bold p-3 font-heading">Testimonials</h1>
+    <section
+      className="relative p-6 md:p-16 w-full text-center bg-Backgroundwhite"
+      data-scroll-section
+    >
+      <h1 className="text-3xl md:text-5xl font-bold p-3 font-heading">
+        Testimonials
+      </h1>
       <Swiper
         freeMode={true}
         grabCursor={true}
@@ -19,10 +48,41 @@ const Testimonial = () => {
         pagination={{ clickable: true }}
         modules={[Pagination]}
       >
-        <SwiperSlide>
-          <div className="p-6">
-            <div className="text-center myTest">
-              <p className="text-xl md:text-3xl mb-4 pb-4">
+        {testimonial.map((testimonial, index) => 
+          <SwiperSlide key={index}>
+          <img
+            src="./images/Test_mobile.png"
+            alt="bg"
+            className="lg:hidden flex relative w-full object-cover "
+          />
+          <img
+            src="./images/Testimonial-background.svg"
+            alt="bg"
+            className="hidden lg:flex relative w-full px-12 object-cover"
+          />
+          <div className="p-6 flex justify-center items-center">
+            <div className="absolute max-w-[750px] top-6 md:top-44 lg:top-10 xl:top-20 text-center myTest">
+              <p className="text-lg md:text-3xl lg:text-xl xl:text-2xl p-4 font-medium text-lightBlack">
+                {testimonial.content}
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        )}
+        {/* <SwiperSlide>
+          <img
+            src="./images/Test_mobile.png"
+            alt="bg"
+            className="lg:hidden flex relative w-full object-cover "
+          />
+          <img
+            src="./images/Testimonial-background.svg"
+            alt="bg"
+            className="hidden lg:flex relative w-full px-12 object-cover"
+          />
+          <div className="p-6 flex justify-center items-center">
+            <div className="absolute max-w-[750px] top-6 md:top-44 lg:top-10 xl:top-20 text-center myTest">
+              <p className="text-lg md:text-3xl lg:text-xl xl:text-2xl p-4 font-medium text-lightBlack">
                 My experience with Mynd has been really smooth and helpful. I’m
                 happy I decided to reach out to them for their services and
                 since the beginning they’ve been really involved and proactive.
@@ -35,17 +95,27 @@ const Testimonial = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="p-6">
-            <div className="text-center myTest">
-              <p className="text-xl md:text-3xl mb-4 pb-4">
+          <img
+            src="./images/Test_mobile.png"
+            alt="bg"
+            className="lg:hidden flex relative w-full object-cover "
+          />
+          <img
+            src="./images/Testimonial-background.svg"
+            alt="bg"
+            className="hidden lg:flex relative w-full px-12 object-cover"
+          />
+          <div className="p-6 flex justify-center items-center">
+            <div className="absolute max-w-[750px] top-6 md:top-44 lg:top-10 xl:top-20 text-center myTest">
+              <p className="text-lg md:text-3xl lg:text-xl xl:text-2xl p-4 font-medium text-lightBlack">
                 I was going through a really tough time and I was really
                 struggling to find the help I needed until I found Mynd. They
                 were so amazingly understanding, efficient and responsive, and
                 made sure I got the help I needed as quickly as possible. Their
                 discounted student rates were also incredibly helpful for me and
-                it made it much easier for me to seek help. I definitely recommend
-                seeking help through Mynd, it changed a lot of things for me and
-                I’m very grateful for it.
+                it made it much easier for me to seek help. I definitely
+                recommend seeking help through Mynd, it changed a lot of things
+                for me and I’m very grateful for it.
               </p>
             </div>
           </div>
@@ -53,7 +123,7 @@ const Testimonial = () => {
         <SwiperSlide>
           <div className="p-6">
             <div className="text-center myTest">
-              <p className="text-xl md:text-3xl mb-4 pb-4">
+              <p className="text-xl md:text-3xl pb-4">
                 Mynd helped me with student-pocket friendly therapy session when
                 I was in a rough spot. The team replied immediately and assigned
                 a session (with an amazing psychologist/therapist) at my
@@ -68,7 +138,7 @@ const Testimonial = () => {
         <SwiperSlide>
           <div className="p-6">
             <div className="text-center myTest">
-              <p className="text-xl md:text-3xl mb-4 pb-4">
+              <p className="text-xl md:text-3xl pb-4">
                 My experience with mynd was wonderful. In a low point in my life
                 and mindset, Ms. Vedika really was a source of self-reflection
                 and understanding. Mynd also made the whole customer experience
@@ -83,7 +153,7 @@ const Testimonial = () => {
         <SwiperSlide>
           <div className="p-6">
             <div className="text-center myTest">
-              <p className="text-xl md:text-3xl mb-4 pb-4">
+              <p className="text-xl md:text-3xl pb-4">
                 At first, the thought of online therapy was a little scary. The
                 thought of not having an in person meeting with someone who I
                 was to share very personal experiences with made me anxious.
@@ -94,7 +164,7 @@ const Testimonial = () => {
               </p>
             </div>
           </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
       </Swiper>
     </section>
   );
