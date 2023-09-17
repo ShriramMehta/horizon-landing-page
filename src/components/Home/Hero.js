e1705f1dad31a3680b330d524233fc6a96381da4
Siddhi-S <114423987+Siddhi-Sheth@users.noreply.github.com>
2023-09-17 21:12:43 +0530

    transitions

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { motion as m, useScroll, useAnimation } from "framer-motion";
+
+// const item = {
+//   // hidden: { x: "10vh", opacity: 0 },
+//   visible: {
+//     y: 0,
+//     opacity: 1,
+//   },
+// };
 
 const Hero = () => {
   return (
     <section className="mx-auto mb-auto xl:mb-44">
       <div className="flex flex-wrap-reverse md:flex-nowrap w-full justify-between items-center">
         <div className="w-full md:w-1/2 xl:w-1/2  flex flex-col justify-start items-start p-5 md:ml-12">
+        <m.h1
+              initial={{ y: -30, opacity: 0 }}
+              whileInView={{
+                y: 0,
+                opacity: 1,
+                transition: {
+                  duration: 1.0,
+                  delay: 0.2,
+                  ease: "anticipate",
+                },
+              }}
+            >
           <h2 class="my-4 text-4xl md:text-5xl lg:text-6xl text-textColor font-bold p-3 font-heading">
             <span
               style={{
@@ -20,12 +41,24 @@ const Hero = () => {
             </span>
             <span style={{ color: "#9E9EFF", fontFamily: 'serif' }}>Your Way.</span>
           </h2>
+          </m.h1>
           {/* <img src="./images/header.png" alt="header" className="w-full object-cover h-[50px] md:h-[100px]" /> */}
-          <h4 class="ml-2 my-2 text-[24px] text-headingColor font-[700] p-3 text-left font-body leading-[36px]">
+          <m.h4
+              initial={{ x: -40, opacity: 0 }}
+              whileInView={{
+                x: 0,
+                opacity: 1,
+                transition: {
+                  duration: 1.5,
+                  delay: 0.8,
+                  ease: "anticipate",
+                },
+              }}
+            class="ml-2 my-2 text-[24px] text-headingColor font-[700] p-3 text-left font-body leading-[36px]">
             Your journey to mental well-being starts here.
             <br />
             Let's create a happier, healthier you - together!
-          </h4>
+          </m.h4>
           <h4 class="ml-2 my-1 text-[22px] font-normal text-textColor p-3 text-left font-body leading-[36px]">
             Wellness isn’t a one size fits all concept.
             <br />
@@ -34,12 +67,32 @@ const Hero = () => {
           </h4>
         </div>
         <div className="w-full md:w-1/2 lg:w-1/4 xl:w-1/2 flex justify-end items-end pl-4">
-          <img
+          <m.img
+          initial={{ x: 30, opacity: 0 }}
+          whileInView={{
+            x: 0,
+            opacity: 1,
+            transition: {
+              duration: 1.1,
+              delay: 1.1,
+              ease: "anticipate",
+            },
+          }}
             src="./images/hero_back1.png"
             alt="hero"
             className="w-full md:absolute md:w-1/2 h-auto top-[10rem] lg:top-[-8rem] xl:top-[-14rem]  z-[-1]"
           />
-          <img
+          <m.img
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 1.5,
+              delay: 1.5,
+              ease: "anticipate",
+            },
+          }}
             src="./images/hero.png"
             alt="mind"
             className="absolute top-[16rem] md:top-[18rem] lg:top-10 xl:top-0 w-full md:w-1/2 z-[1]"
