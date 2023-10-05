04e21b34a83bfda6a19c723eb400094e845500e5
Siddhi-S <114423987+Siddhi-Sheth@users.noreply.github.com>
2023-10-05 19:58:12 +0530

    team

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -4,23 +4,23 @@ const Founders = [
   {
     name: "Aayushi Shah",
     image: "./images/img1.png",
-    para: "“The day people talk about and confront mental health issues,therapy, seek help for themselves & make it a part of their lifestyle, is the day my goals would be fulfilled.”",
-    subpara:
-      "With a passion to help and contribute towards people’s lives, Aayushi aims to invest her learnings towards the growth of mynd and help as many people as possible.",
+    // para: "“The day people talk about and confront mental health issues,therapy, seek help for themselves & make it a part of their lifestyle, is the day my goals would be fulfilled.”",
+    // subpara:
+    //   "With a passion to help and contribute towards people’s lives, Aayushi aims to invest her learnings towards the growth of mynd and help as many people as possible.",
   },
   {
     name: "Preet Bhatti",
     image: "./images/img2.png",
-    para: "“There is a need to acknowledge the importance of investing in yourself, mentally & physically – time, resources, energy, everything!”",
-    subpara:
-      "With a strong sense of empathy and desire to help individuals gain the guidance they require, Preet approaches her contributions towards Mynd in a realistic and creative way.",
+    // para: "“There is a need to acknowledge the importance of investing in yourself, mentally & physically – time, resources, energy, everything!”",
+    // subpara:
+    //   "With a strong sense of empathy and desire to help individuals gain the guidance they require, Preet approaches her contributions towards Mynd in a realistic and creative way.",
   },
   {
     name: "Varnika Hathi",
     image: "./images/img3.png",
-    para: "“The aim is to create a community. A community of those who provide help, and those who seek it. When people know there’s a place they can go to in times of need, our aim will be fulfilled.”",
-    subpara:
-      "With a focus on psychology in the digital world, Varnika believes in extending the services of Mynd to as many people, making it as accessible as possible!",
+    // para: "“The aim is to create a community. A community of those who provide help, and those who seek it. When people know there’s a place they can go to in times of need, our aim will be fulfilled.”",
+    // subpara:
+    //   "With a focus on psychology in the digital world, Varnika believes in extending the services of Mynd to as many people, making it as accessible as possible!",
   },
 ];
 
@@ -38,19 +38,19 @@ const Team = () => {
           Founders
         </h1>
       </div>
-      <div className="flex flex-col md:flex-row w-full justify-center items-center p-6 rounded-[50px] shadow-purpleShadow ">
-        <div className="md:p-6 relative flex  gap-6 justify-between items-center w-full">
-          <div className="flex flex-row flex-wrap gap-6 justify-center items-center">
+      <div className="flex flex-col md:flex-col w-full justify-center text-center items-center p-6 rounded-[50px] shadow-purpleShadow ">
+        <div className="md:p-6 relative flex  justify-between items-center w-full gap-6">
+          <div className="w-full flex flex-row flex-wrap gap-12 justify-center items-center">
           {Founders.map((founderData, index) => (
             <div
               key={index}
-              className="w-[200px] h-[200px]"
+              className="w-[250px] h-[250px]"
               onMouseOver={() => setFounder(index)}
             >
               <img
                 src={founderData.image}
                 alt="founder"
-                className={`max-w-full max-h-full cursor-pointer transform transition-transform duration-300 hover:scale-110 ${
+                className={`w-full max-w-full max-h-full cursor-pointer transform transition-transform duration-300 hover:scale-110 ${
                   founder === index ? "" : "grayscale"
                 }`}
               />
@@ -58,8 +58,8 @@ const Team = () => {
           ))}
           </div>
         </div>
-        <div className="flex text-textColor flex-col justify-center p-6 text-left gap-3 w-full">
-          <h1 className="text-4xl font-heading font-bold my-6">
+        <div className="flex text-textColor flex-col justify-center items-center p-6 text-left gap-3 w-full">
+          <h1 className="text-5xl text-center font-heading font-bold my-6">
             {Founders[founder].name}
           </h1>
           <h4 className="text-xl font-heading font-semibold mb-3">
