77fb608e7f124d30cc0b2cdfc1011401bd8b9093
Siddhi-S <114423987+Siddhi-Sheth@users.noreply.github.com>
2023-11-11 20:43:48 +0530

    book session

--- src/pages/BookSession.js
+++ src/pages/BookSession.js
@@ -30,9 +30,13 @@ const BookSession = () => {
 
   return (
     <div className="flex flex-col max-w-screen-lg h-screen mx-auto">
-      <div>Book Session</div>
+      {/* <div>
+        <p className="text-[#101828] text-2xl font-semibold text-center py-1">
+          Book Session
+        </p>
+      </div> */}
       <div className="shadow-xl flex flex-col gap-4 p-6">
-        <div className="flex justify-center items-center">
+        <div className="flex flex-col md:flex-row justify-center items-center">
           <div className="flex justify-center items-center gap-4 max-w-1/2 w-full h-5/6 shadow-lg p-4">
             <img
               className="w-36 h-36 object-cover rounded-full shadow-lg"
@@ -59,7 +63,7 @@ const BookSession = () => {
                   <img
                     src="./images/Chevron right.png"
                     alt="right"
-                    className="w-[30px] h-[30px]"
+                    className="w-[100px] md:w-[30px] h-[30px]"
                   />
                 </div>
               </div>
@@ -74,29 +78,44 @@ const BookSession = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-4">
-          <p className="text-[#101828] text-xl font-semibold">
-            Session Pricing
-          </p>
-          <div className="flex gap-4">
-            <div className="flex flex-row justify-center items-center font-semibold text-[#101828]">
-              <img src="./images/currency-rupee.png" alt="rupee" />
-              <span>800/hr</span>
-            </div>
-            <div className="max-w-[130px] w-full max-h-[30px] h-full rounded-[16px] flex justify-center items-center py-[2px] px-[6px] bg-[#F0F0FE] border-[1px] border-[##EAECF0]">
-              <div className="flex flex-row justify-start items-center gap-2">
-                <p className="text-xs font-semibold text-[#4E139F] text-center">
-                  Student Status
-                </p>
-                <img src="./images/Check Circle.png" alt="check" />
-              </div>
-            </div>
-          </div>
-        </div>
         <div>
           <hr></hr>
         </div>
-        <div>NOte</div>
+        <div className="flex flex-col  md:flex-row md:justify-end md:mr-[30px] gap-12 items-center">
+          <div className="flex flex-col gap-4 w-full md:w-auto">
+            <p className="text-[#101828] text-xl font-semibold">
+              Session Pricing
+            </p>
+            <div className="flex gap-4">
+              <div className="flex flex-row justify-center items-center font-semibold text-[#101828]">
+                <img src="./images/currency-rupee.png" alt="rupee" />
+                <span>800/hr</span>
+              </div>
+              <div className="max-w-[130px] w-full max-h-[30px] h-full rounded-[16px] flex justify-center items-center py-[2px] px-[6px] bg-[#F0F0FE] border-[1px] border-[##EAECF0]">
+                <div className="flex flex-row justify-start items-center gap-2">
+                  <p className="text-xs font-semibold text-[#4E139F] text-center">
+                    Student Status
+                  </p>
+                  <img src="./images/Check Circle.png" alt="check" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full md:w-auto">
+            <button className="text-center bg-primaryIndigo hover:bg-lightBlue text-white px-4 py-2 rounded-full mr-2 flex justify-center items-center w-full  md:w-auto">
+              Book a Session
+              <img
+                src="./images/Chevron right white.png"
+                alt="right"
+                // className="w-[30px] h-[30px]"
+              />
+            </button>{" "}
+          </div>
+        </div>
+        <div className="my-2 md:my-0 text-sm text-[#667085] h-[20px] bg-white w-full text-center rounded-br-[16px] rounded-bl-[16px]">
+          Please Note: You will receive the google meet link for the session
+          after the payment is completed.
+        </div>
       </div>
     </div>
   );
