6f84babd6f87cd54302a9dadc81cb909c209b5eb
Siddhi-S <114423987+Siddhi-Sheth@users.noreply.github.com>
2023-11-20 19:40:41 +0530

    name filter

---
+++ src/components/TherapistFilterModal.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+const TherapistFilterModal = ({ applyFilters }) => {
+    const [areaOfFocus, setAreaOfFocus] = useState("");
+    const [experience, setExperience] = useState("");
+    const [gender, setGender] = useState("");
+    const [languages, setLanguages] = useState([]);
+    const [approach, setApproach] = useState([]);
+  
+    const handleApplyFilters = () => {
+      const filters = {
+        areaOfFocus,
+        experience,
+        gender,
+        languages,
+        approach,
+      };
+      applyFilters(filters);
+    };
+  
+    return (
+      <div className="shadow-xl flex flex-col justify-center items-center gap-4 p-6 max-w-screen-md">
+        {/* Add your filter options here */}
+        <label>
+          Area of Focus:
+          <input
+            type="text"
+            value={areaOfFocus}
+            onChange={(e) => setAreaOfFocus(e.target.value)}
+          />
+        </label>
+        <label>
+          Years of Experience:
+          <input
+            type="text"
+            value={experience}
+            onChange={(e) => setExperience(e.target.value)}
+          />
+        </label>
+        <label>
+          Gender:
+          <input
+            type="text"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          />
+        </label>
+        <label>
+          Languages:
+          <select
+            multiple
+            value={languages}
+            onChange={(e) => setLanguages(Array.from(e.target.selectedOptions, (option) => option.value))}
+          >
+            <option value="Hindi">Hindi</option>
+            <option value="English">English</option>
+            <option value="Marathi">Marathi</option>
+          </select>
+        </label>
+        <label>
+          Approach:
+          <select
+            multiple
+            value={approach}
+            onChange={(e) => setApproach(Array.from(e.target.selectedOptions, (option) => option.value))}
+          >
+            <option value="CBT">CBT</option>
+            <option value="DBT">DBT</option>
+            <option value="REBT">REBT</option>
+            <option value="FamilyTherapy">Family Therapy</option>
+            <option value="ArtTherapy">Art Therapy</option>
+            <option value="Others">Others</option>
+          </select>
+        </label>
+        <button onClick={handleApplyFilters}>Apply</button>
+      </div>
+    );
+  };
+
+export default TherapistFilterModal
--- src/pages/Therapist.js
+++ src/pages/Therapist.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import TherapistFilterModal from "../components/TherapistFilterModal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faFilter } from "@fortawesome/free-solid-svg-icons";
+
 
 const TherapistData = [
   {
@@ -13,7 +17,7 @@ const TherapistData = [
   {
     img: "./images/img2.png",
     name: "Dr. John Smith",
-    exp: "10+ years",
+    exp: "5+ years",
     fees: "$100",
     special:
       "Stress,Relationship,Self development,Anxiety,Work related,Sleep help",
@@ -36,7 +40,7 @@ const TherapistData = [
   },
   {
     img: "./images/img2.png",
-    name: "Dr. John Smith",
+    name: "Dr. Siddhi ABC",
     exp: "10+ years",
     fees: "$100",
     special:
@@ -54,174 +58,145 @@ const TherapistData = [
 
 const Therapist = () => {
   const navigate = useNavigate();
-
+  const [filteredTherapists, setFilteredTherapists] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   function handleBookSession() {
-    // const token = localStorage.getItem("token");
-    // if (token) {
-    //   console.log("user is logged in");
-    //   navigate("/bookSession");
-    // } else {
-    //   console.log("user is not looged in");
-    //   navigate("/login");
-    // }
     navigate("/bookSession");
   }
   function handleViewDetials() {
-    // const token = localStorage.getItem("token");
-    // if (token) {
-    //   console.log("user is logged in");
-    //   navigate("/bookSession");
-    // } else {
-    //   console.log("user is not looged in");
-    //   navigate("/login");
-    // }
     navigate("/viewDetails");
   }
 
+  const handleInputChange = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    const filtered = TherapistData.filter((therapist) =>
+      String(therapist.name).toLowerCase().includes(term.toLowerCase())
+    );
+    setFilteredTherapists(filtered);
+  };
+
   return (
     <div className="p-3 max-w-screen-xl h-full flex flex-col justify-center items-center gap-12 mx-auto">
-      <div>Filter</div>
+      <div className="flex flex-row gap-4 w-full justify-center">
+      <div className="relative">
+        <input
+          placeholder="Search Thearpist"
+          className="relative border px-10 py-3 rounded-[16px] border-black"
+          value={searchTerm}
+          onChange={handleInputChange}
+        />
+        <FontAwesomeIcon
+          icon={faSearch}
+          className="absolute left-[10px] top-[16px] text-gray-500"
+        />
+      </div>
+      <div className="cursor-pointer relative flex gap-3 items-center border-black border px-4 py-2 rounded-[16px]">
+        <FontAwesomeIcon
+          icon={faFilter}
+          className="text-gray-500"
+        />
+        <p>Filters</p>
+      </div>
+      </div>
       <div className="flex p-3 flex-row gap-8 md:gap-4 justify-center xl:justify-between flex-wrap w-full">
-        <div className="max-w-[400px] w-full bg-white shadow-xl rounded-2xl overflow-hidden">
-          <div className="flex flex-col">
-            <div className="flex justify-center items-center gap-4 max-w-1/2 w-full h-5/6 p-4">
-              <img
-                className="w-36 h-36 object-cover"
-                src="./images/img2.png"
-                alt="Therapist Photo"
-              />
-              <div className="max-w-1/2 w-full flex flex-col gap-1">
-                <p className="text-[#101828] text-lg font-semibold">
-                  Dr. John Smith
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Experience: 10+ years
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Hourly Fees: $100
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Appoitment mode:Online via google meet
-                </p>
+        {filteredTherapists.length > 0
+          ? filteredTherapists.map((item, idx) => (
+              <div
+                key={idx}
+                className="max-w-[400px] w-full bg-white shadow-xl rounded-2xl overflow-hidden"
+              >
+                <div className="flex flex-col">
+                  <div className="flex justify-center items-center gap-4 max-w-1/2 w-full h-5/6 p-4">
+                    <img
+                      className="w-36 h-36 object-cover"
+                      src={item.img}
+                      alt="Therapist Photo"
+                    />
+                    <div className="max-w-1/2 w-full flex flex-col gap-1">
+                      <p className="text-[#101828] text-lg font-semibold">
+                        {item.name}
+                      </p>
+                      <p className="text-[#475467] text-base font-normal">
+                        Experience: {item.exp}
+                      </p>
+                      <p className="text-[#475467] text-base font-normal">
+                        Hourly Fees: {item.fees}
+                      </p>
+                      <p className="text-[#475467] text-base font-normal">
+                        Appoitment mode:Online via google meet
+                      </p>
+                    </div>
+                  </div>
+                  <div className="px-4">
+                    <h1 className="text-black font-semibold">Best help for:</h1>
+                    <p className="text-gray-600 mb-2">{item.special}</p>
+                    <button
+                      className="my-4 bg-primaryIndigo hover:bg-blue-600 text-white px-4 py-2 rounded-full mr-2"
+                      onClick={() => {
+                        handleBookSession();
+                      }}
+                    >
+                      Book a Session
+                    </button>
+                    <button
+                      onClick={() => navigate("/viewDetails")}
+                      className="my-4 bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded-full"
+                    >
+                      View Details
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="px-4">
-              <h1 className="text-black font-semibold">Best help for:</h1>
-              <p className="text-gray-600 mb-2">
-                Stress, Relationship, Self development, Anxiety, Work related,
-                Sleep help
-              </p>
-              <button
-                className="my-4 bg-primaryIndigo hover:bg-blue-600 text-white px-4 py-2 rounded-full mr-2"
-                onClick={() => {
-                  handleBookSession();
-                }}
+            ))
+          : TherapistData.map((item, idx) => (
+              <div
+                key={idx}
+                className="max-w-[400px] w-full bg-white shadow-xl rounded-2xl overflow-hidden"
               >
-                Book a Session
-              </button>
-              <button
-                onClick={() => navigate("/viewDetails")}
-                className="my-4 bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded-full"
-              >
-                View Details
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-[400px] w-full bg-white shadow-xl rounded-2xl overflow-hidden">
-          <div className="flex flex-col">
-            <div className="flex justify-center items-center gap-4 max-w-1/2 w-full h-5/6 p-4">
-              <img
-                className="w-36 h-36 object-cover"
-                src="./images/img2.png"
-                alt="Therapist Photo"
-              />
-              <div className="max-w-1/2 w-full flex flex-col gap-1">
-                <p className="text-[#101828] text-lg font-semibold">
-                  Dr. John Smith
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Experience: 10+ years
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Hourly Fees: $100
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Appoitment mode:Online via google meet
-                </p>
+                <div className="flex flex-col">
+                  <div className="flex justify-center items-center gap-4 max-w-1/2 w-full h-5/6 p-4">
+                    <img
+                      className="w-36 h-36 object-cover"
+                      src={item.img}
+                      alt="Therapist Photo"
+                    />
+                    <div className="max-w-1/2 w-full flex flex-col gap-1">
+                      <p className="text-[#101828] text-lg font-semibold">
+                        {item.name}
+                      </p>
+                      <p className="text-[#475467] text-base font-normal">
+                        Experience: {item.exp}
+                      </p>
+                      <p className="text-[#475467] text-base font-normal">
+                        Hourly Fees: {item.fees}
+                      </p>
+                      <p className="text-[#475467] text-base font-normal">
+                        Appoitment mode:Online via google meet
+                      </p>
+                    </div>
+                  </div>
+                  <div className="px-4">
+                    <h1 className="text-black font-semibold">Best help for:</h1>
+                    <p className="text-gray-600 mb-2">{item.special}</p>
+                    <button
+                      className="my-4 bg-primaryIndigo hover:bg-blue-600 text-white px-4 py-2 rounded-full mr-2"
+                      onClick={() => {
+                        handleBookSession();
+                      }}
+                    >
+                      Book a Session
+                    </button>
+                    <button
+                      onClick={() => navigate("/viewDetails")}
+                      className="my-4 bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded-full"
+                    >
+                      View Details
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="px-4">
-              <h1 className="text-black font-semibold">Best help for:</h1>
-              <p className="text-gray-600 mb-2">
-                Stress, Relationship, Self development, Anxiety, Work related,
-                Sleep help
-              </p>
-              <button
-                className="my-4 bg-primaryIndigo hover:bg-blue-600 text-white px-4 py-2 rounded-full mr-2"
-                onClick={() => {
-                  handleBookSession();
-                }}
-              >
-                Book a Session
-              </button>
-              <button
-                onClick={() => navigate("/viewDetails")}
-                className="my-4 bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded-full"
-              >
-                View Details
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-[400px] w-full bg-white shadow-xl rounded-2xl overflow-hidden">
-          <div className="flex flex-col">
-            <div className="flex justify-center items-center gap-4 max-w-1/2 w-full h-5/6 p-4">
-              <img
-                className="w-36 h-36 object-cover"
-                src="./images/img2.png"
-                alt="Therapist Photo"
-              />
-              <div className="max-w-1/2 w-full flex flex-col gap-1">
-                <p className="text-[#101828] text-lg font-semibold">
-                  Dr. John Smith
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Experience: 10+ years
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Hourly Fees: $100
-                </p>
-                <p className="text-[#475467] text-base font-normal">
-                  Appoitment mode:Online via google meet
-                </p>
-              </div>
-            </div>
-            <div className="px-4">
-              <h1 className="text-black font-semibold">Best help for:</h1>
-              <p className="text-gray-600 mb-2">
-                Stress, Relationship, Self development, Anxiety, Work related,
-                Sleep help
-              </p>
-              <button
-                className="my-4 bg-primaryIndigo hover:bg-blue-600 text-white px-4 py-2 rounded-full mr-2"
-                onClick={() => {
-                  handleBookSession();
-                }}
-              >
-                Book a Session
-              </button>
-              <button
-                onClick={() => {
-                  handleViewDetials();
-                }}
-                className="my-4 bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded-full"
-              >
-                View Details
-              </button>
-            </div>
-          </div>
-        </div>
+            ))}
       </div>
     </div>
   );
