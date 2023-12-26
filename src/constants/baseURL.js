d2e287e6d5d9b4419f9d519202195eb4c817dac5
ShriramMehta <shrirammehtasrm@gmail.com>
2023-12-26 21:54:14 +0530

    base

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,3 +1,3 @@
-export default "http://localhost:5000/api";
+// export default "http://localhost:5000/api";
 // export default "http://13.51.102.165:5000/api";
-// export default "https://adaptwellness.in/api";
+export default "https://adaptwellness.in/api";
