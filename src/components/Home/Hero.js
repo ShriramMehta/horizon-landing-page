a65a04d16956dcdf13c35ed0749daebdc9e7e544
ShriramMehta <shrirammehtasrm@gmail.com>
2024-01-31 13:36:29 +0530

    asd

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -99,7 +99,7 @@ const Hero = () => {
             }}
             src="/images/3.png"
             alt="mind"
-            className="absolute top-[16rem] md:top-[18rem] lg:top-10 xl:top-0  sm:w-[95%] md:w-1/2"
+            className="absolute top-[16rem] md:top-[18rem] lg:top-10 xl:top-0  w-[95%] md:w-1/2"
           />
         </div>
       </div>
